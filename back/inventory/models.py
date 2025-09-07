e4fa166330540a92113d797660523f557dda4874
BarfleurLight <obrishtigoogl@gmail.com>
2025-09-07 17:15:09 +0300

    feat(inventory): add models Responsible, IP, Consumables

--- back/inventory/models.py
+++ back/inventory/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
-# Create your models here.
+
+class Responsible(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Имя')
+    surname = models.CharField(max_length=200, verbose_name='Фамилия')
+    patronymic = models.CharField(
+        max_length=200,
+        verbose_name='Отчество',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Ответственный'
+        verbose_name_plural = 'Ответственные'
+
+    def __str__(self) -> str:
+        return self.surname
+
+
+class IP(models.Model):
+    ip = models.GenericIPAddressField(
+        protocol='IPv4',
+        unique=True,
+        verbose_name='IPv4'
+    )
+
+    class Meta:
+        verbose_name = 'IPv4-адрес'
+        verbose_name_plural = 'IPv4-адреса'
+
+    def __str__(self) -> str:
+        return self.ip
+
+
+class Consumables(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Название')
+    cons_type = models.CharField(max_length=200, verbose_name='Тип расходника')
+    count = models.IntegerField(default=0, verbose_name="Количество")
+    image = models.ImageField(
+        upload_to='consumables/',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Расходник'
+        verbose_name_plural = 'Расходники'
+
+    def __str__(self) -> str:
+        return self.name
