6d4ff8d1b5872aa6fceb29213288436c56a4eec4
BarfleurLight <obrishtigoogl@gmail.com>
2025-09-12 10:28:00 +0300

    feat(docker): init docker files and workflow

--- back/socio_inventory_back/settings.py
+++ back/socio_inventory_back/settings.py
@@ -86,10 +86,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/backend_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/backend_media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
