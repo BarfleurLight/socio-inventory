77b89fee771d7cdb0c0926661949184a49f6fb41
BarfleurLight <obrishtigoogl@gmail.com>
2025-09-15 15:13:03 +0300

    feat(api): add paginations for inventory, consumables

--- back/api/views.py
+++ back/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from .pagination import CustomPaginations
 from inventory.models import Models, Consumables, Inventory
 from .serializers import (ModelSerializer, ConsumablesSerializer,
                           InventoryListSerializer, InventoryWriteSerializer)
@@ -11,11 +12,13 @@ class ModelViewSet(viewsets.ReadOnlyModelViewSet):
 
 class ConsumablesViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Consumables.objects.all()
+    pagination_class = CustomPaginations
     serializer_class = ConsumablesSerializer
 
 
 class InventoryViewSet(viewsets.ModelViewSet):
     queryset = Inventory.objects.all()
+    pagination_class = CustomPaginations
     
     def get_serializer_class(self):
         if self.action in ('list', 'retrieve'):
