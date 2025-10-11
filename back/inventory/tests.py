f23c6f4e1b33330255f1ef1d31d196437f09c5cf
BarfleurLight <obrishtigoogl@gmail.com>
2025-10-11 16:13:17 +0300

    feat(tests): add MAC model tests

--- back/inventory/tests.py
+++ back/inventory/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from inventory.models import Responsible
+from django.core.exceptions import ValidationError
+from inventory.models import Responsible, Inventory, MAC, IP
 
 
 class ResponsibleModelTest(TestCase):
@@ -45,3 +46,89 @@ class ResponsibleModelTest(TestCase):
         expected_full_name = '  Сидорова     Анна     Владимировна  '
         self.assertEqual(responsible.get_full_name(), expected_full_name)
         self.assertEqual(str(responsible), expected_full_name)
+
+
+class MACModelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        cls.inventory = Inventory.objects.create(
+            full_name='Test Inventory',
+            serial_number='000123123123'
+        )
+
+        cls.ip = IP.objects.create(ip='192.168.1.1')
+
+    def test_create_mac_with_required_fields_only(self):
+        """Generate MAC address with required fields only."""
+        mac_data = {
+            'mac': '00:1B:44:11:3A:B7',
+            'interface': 'ethernet'
+        }
+        
+        mac = MAC.objects.create(**mac_data)
+        
+        self.assertEqual(mac.mac, '00:1b:44:11:3a:b7')
+        self.assertEqual(mac.interface, 'ethernet')
+        self.assertIsNone(mac.inventory)
+        self.assertEqual(mac.ip_addresses.count(), 0)
+
+    def test_create_mac_with_all_fields(self):
+        """Generate MAC address with all fields only."""
+        mac_data = {
+            'mac': '00:1B:44:11:3A:B7',
+            'interface': 'ethernet',
+            'inventory': MACModelTest.inventory
+        }
+        
+        mac = MAC.objects.create(**mac_data)
+        mac.ip_addresses.set([MACModelTest.ip,])
+
+        self.assertEqual(mac.mac, '00:1b:44:11:3a:b7')
+        self.assertEqual(mac.interface, 'ethernet')
+        self.assertEqual(mac.inventory, MACModelTest.inventory)
+        self.assertEqual(mac.ip_addresses.count(), 1)
+
+    def test_valid_mac_address_formats(self):
+        """Checking valid MAC address formats."""
+        valid_macs = [
+            '00:1B:44:11:3A:B7',
+            '00-1B-44-11-3A-B7',
+            'a1:b2:c3:d4:e5:f6',
+            'A1-B2-C3-D4-E5-F6',
+        ]
+        
+        for valid_mac in valid_macs:
+            with self.subTest(mac=valid_mac):
+                mac = MAC(
+                    mac=valid_mac,
+                    interface='ethernet'
+                )
+                try:
+                    mac.full_clean()
+                except ValidationError:
+                    self.fail(
+                        f"The MAC address {valid_mac} failed validation."
+                    )
+
+    def test_invalid_mac_address_formats(self):
+        """Checking for invalid MAC address formats."""
+        invalid_macs = [
+            '00:1B:44:11:3A:B7:XX',  
+            '00:1B:44:11:3A',        
+            '00:1B:44:11:3A:BG',     
+            '001B44113AB7',          
+            '00.1B.44.11.3A.B7',     
+            '',                      
+            '  ',                    
+        ]
+        
+        for invalid_mac in invalid_macs:
+            with self.subTest(mac=invalid_mac):
+                mac = MAC(
+                    mac=invalid_mac,
+                    interface='ethernet'
+                )
+                with self.assertRaises(ValidationError):
+                    mac.full_clean()
